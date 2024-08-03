01c643cf8f8ac02232289f8f6d48adc53c51b7e4
Eng.Saeed Balkhair <s.balkhair@hotmail.com>
2024-08-03 09:19:05 +0300

    crate full website

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function runTypingEffect()
+{
+    const text = "I'am Saeed Balkhair"
+    const typingElement = document.getElementById("typing-text")
+    const typingDelay = 100;
+
+    typeText(text, typingElement, typingDelay);
+}
+
+function typeText(text, typingElement, delay)
+{
+    for (let i = 0; i < text.length; i++) {
+        setTimeout(() =>
+        {
+            typingElement.textContent += text.charAt(i);
+        }, delay * i);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", runTypingEffect);
